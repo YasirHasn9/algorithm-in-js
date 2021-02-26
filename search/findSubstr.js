0d97726e085bf85ac38cff3475c32b075e0fbb77
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-26 10:32:53 -0800

    search for substring in long word or quote

---
+++ search/findSubstr.js
@@ -0,0 +1,12 @@
+function findSubstr(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+console.log(findSubstr("my name is yasir", "yasir"));
