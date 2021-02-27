50182226d76f3ffb1dc08ff55ef24b08778a2d2c
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-26 16:26:28 -0800

    bubble sort

---
+++ sorting/bubble.js
@@ -0,0 +1,19 @@
+// SORTING BUBBLE
+// It is the process to compare each 2 items in an array and if one
+// of them is larger then the other then we swap it
+// on first loop we will guarantee the larger item will be on the end
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(bubbleSort([3, 2, 22, 1, 10, 0]));
---
+++ sorting/readme.md
@@ -0,0 +1,11 @@
+what is sorting ?
+Is the process of rearrange items in any order.
+Examples
+sorting numbers from small to large
+sorting names alphabetically
+sorting movies
+
+why we need to know sorting?
+
+1. because it is a common task in programming, it is good to know how it works
+2. there are many different ways to sort things, each technique has its own advantage
