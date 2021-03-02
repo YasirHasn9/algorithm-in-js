ddcf98fc9aef96c69c39ca0f1d45862593afd225
Yasir Hassan <hasanyasir01@gmail.com>
2021-03-02 10:26:34 -0800

    imporving the sorting solution

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -5,8 +5,8 @@
 
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      console.log(arr, arr[j], arr[j + 1]);
+    for (let j = 0; j < arr.length - 1; j++) {
+      // console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -17,15 +17,17 @@ function bubbleSort(arr) {
   return arr;
 }
 function betterBubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = arr.length; i > 0; i--) {
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
-        arr[j + 1] = arr[j];
+        arr[j + 1] = temp;
       }
     }
   }
   return arr;
 }
+
+console.log(bubbleSort([3, 2, 22, 1, 10, 0]));
 console.log(betterBubbleSort([3, 2, 22, 1, 10, 0]));
