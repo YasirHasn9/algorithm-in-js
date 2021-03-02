e3c8dcd98a17fffebeb4c2bf4d6866147f20b662
Yasir Hassan <hasanyasir01@gmail.com>
2021-03-02 13:38:14 -0800

    O(n) of bubble sort

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -27,14 +27,20 @@ function betterBubbleSort(arr) {
     }
   }
   return arr;
-}
+} // o(n^2)
 
 function bubbleEs6(arr) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
+
+  // we can optimized our solution by adding a bool var
   let noSwap;
   for (let i = arr.length; i > 0; i--) {
+    // we set it to true because we assume we have no swap
+    // this is so useful when we have almost sorted array
+    // because with bubble sorting checking will continue to the end of
+    // array
     noSwap = true;
     for (let j = 0; j < i - 1; j++) {
       // console.log(arr, arr[j], arr[j + 1]);
@@ -46,7 +52,7 @@ function bubbleEs6(arr) {
     if (noSwap) break;
   }
   return arr;
-}
+} // almost O(n)
 
 console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
 console.log(betterBubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
