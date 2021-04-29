3beff94b7349bda74bdb9a2ada0f7c5778b79287
Yasir Hassan <hasanyasir01@gmail.com>
2021-04-29 15:36:03 -0700

    second solution

--- freqCounter.js
+++ freqCounter.js
@@ -10,7 +10,8 @@ let arr2 = [9, 4, 1];
 //    check if the doubled number is not exited in the second array
 //    return false
 //  return true
-function countFreq(arr1, arr2) {
+function countFreq_1(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr2.length; i++) {
     let num = Math.sqrt(arr2[i]);
     if (arr1.indexOf(num) < 0) {
@@ -19,4 +20,16 @@ function countFreq(arr1, arr2) {
   }
   return true;
 }
-console.log(countFreq(arr1, arr2));
+
+function countFreq_2(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    let found = arr2.indexOf(arr1[i] ** 2);
+    if (found < 0) {
+      return false;
+    }
+    arr2.splice(found, 1);
+  }
+  return true;
+}
+console.log(countFreq_2(arr1, arr2));
